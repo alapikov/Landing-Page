b3a2ef556462b433dc5c7c0e40cc0bf4f19c72fd
Alexander Lapikov <racman996@gmail.com>
2022-03-30 14:28:35 +0300

    v1.0.0

--- scriptsTablet.js
+++ scriptsTablet.js
@@ -2,3 +2,8 @@
     const s3ImgsArrows = document.querySelector('#s3ImgsArrows');
     s3ImgsArrows.remove();
 } ());
+
+(function redesignFB3Address() {
+    const fB3Address = document.querySelector('#fB3Address');
+    fB3Address.innerHTML = 'Alpine Str, 14, Melbourne, Australia';
+} ());
